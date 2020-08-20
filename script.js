a9bcb7e8fa8596b1750e14668a32b62ce2030198
Jacky Li <ilikemacaronii@gmail.com>
2020-08-20 15:06:29 -0700

    add dark light theme

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.documentElement.setAttribute('data-theme', 'dark');
+// Constants
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// document.documentElement.setAttribute('data-theme', 'dark');
+
+
+// Sticky Nav
 window.onscroll = () => {stickyNav()};
 
 const nav = document.getElementById('nav');
@@ -10,4 +20,33 @@ function stickyNav() {
   } else {
     nav.classList.remove('sticky');
   }
+}
+
+// Dark/Light Mode Switch
+function switchTheme(evt)  {
+  if(evt.target.checked) {
+    toggleMode(DARK_THEME);
+  } else {
+    toggleMode(LIGHT_THEME);
+  }
+}
+
+function toggleMode(color) {
+  const isLight = 'light' === color;
+  document.documentElement.setAttribute('data-theme', color);
+  (isLight) ?
+    toggleIcon.children[0].classList.replace('fa-moon', 'fa-sun') :
+    toggleIcon.children[0].classList.replace('fa-sun', 'fa-moon');
+  (isLight) ? toggleSwitch.checked=false : toggleSwitch.checked=true;
+  localStorage.setItem('theme', color);
+}
+
+// Event Listeners
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  toggleMode(currentTheme);
 }
