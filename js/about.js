39a0ac3340143198a58ec60380514f0713e2040b
Jacky Li <ilikemacaronii@gmail.com>
2020-09-01 15:34:16 -0700

    add skills set

--- js/about.js
+++ js/about.js
@@ -2,16 +2,16 @@ const skillsBtn = document.getElementById('about-skills-btn');
 const educationBtn = document.getElementById('about-education-btn');
 const personalBtn = document.getElementById('about-personal-btn');
 const aboutBtns = [skillsBtn, educationBtn, personalBtn];
+const aboutInfo = document.getElementById('about-info');
 let currentActive = skillsBtn;
-
-// Dynamic content insertion for about-info
-// for(const btn of aboutBtns) {
-//   console.log(parseInt(btn.value));
-// }
 
 // Show different content depending on which button clicked
 function changeActive(target) {
   if(currentActive !== target) {
+    const prevIndx = parseInt(currentActive.value);
+    const targetIndx = parseInt(target.value);
+    aboutInfo.children[prevIndx].hidden = true;
+    aboutInfo.children[targetIndx].hidden = false;
     currentActive.classList.remove('about-btn-active');
     target.classList.add('about-btn-active');
     currentActive = target;
